968188e0c2b3e2ef823ae7aa7b59d36612fe8ddf
NidaWorld <160415456+nidakhan6@users.noreply.github.com>
2024-03-12 21:58:51 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,33 @@
+//class5
+//let num1 = 10
+//let num2= 5
+//console.log(num1 ==10 && num2 ==5);
+
+
+//et num1 = 10
+//let num2 =5 
+//console.log (!(num1 == num2))
+
+
+let israining = true
+if (israining) {
+    console.log("wear a raincote.");
+}else {
+    console.log("wear sunglasses.");
+}
+
+let num1 = 10
+
+if (num1 == 10){
+    10 ==10
+    console.log("hello i am Nida");
+}
+else {
+    console.log("uder else");
+}
+
+
+
+
+
+
